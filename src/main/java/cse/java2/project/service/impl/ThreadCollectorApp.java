bd0d13accdaef2940a011dacb9bdc9a711f9065d
jimmylaw21 <1908486736@qq.com>
2023-05-12 15:12:51 +0800

    data mining 4th updated

--- src/main/java/cse/java2/project/service/impl/ThreadCollectorApp.java
+++ src/main/java/cse/java2/project/service/impl/ThreadCollectorApp.java
@@ -19,6 +19,85 @@ import java.util.List;
 import java.util.Set;
 
 //添加@Service或@Component注解，以便Spring能够自动检测到它，并将其实例化并添加到容器中
+//@Service
+//public class ThreadCollectorApp {
+//
+//  public static void main(String[] args) {
+//    ThreadCollector threadCollector = new ThreadCollector();
+//    String tag = "java";
+//    int pageSize = 1;
+//    int pageNum = 10;
+//    try {
+//      List<StackOverflowThread> stackOverflowThreads = new ArrayList<>();
+////        for (int i = 1; i <= pageNum; i++) {
+//            stackOverflowThreads.addAll(threadCollector.getStackOverflowThreadsByTag(tag, pageSize, pageNum));
+////        }
+//      String resource = "mybatis-config.xml";
+//      InputStream inputStream = Resources.getResourceAsStream(resource);
+//      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//
+//      SqlSession sqlSession = sqlSessionFactory.openSession();
+//      try {
+//        StackOverflowThreadMapper mapper = sqlSession.getMapper(StackOverflowThreadMapper.class);
+//        //插入用户数据
+//        // 插入用户数据
+//        Set<Owner> users = new LinkedHashSet<>();
+//        for (StackOverflowThread thread : stackOverflowThreads) {
+//          users.add(thread.getQuestion().getOwner());
+//          for (Answer answer : thread.getAnswers()) {
+//            users.add(answer.getOwner());
+//          }
+//          for (Comment comment : thread.getComments()) {
+//            users.add(comment.getOwner());
+//          }
+//        }
+//
+//        for (Owner owner : users) {
+//          if (owner.getUserId() != null) {
+//            try {
+//              mapper.insertOwner(owner);
+//            } catch (DuplicateKeyException e) {
+//              System.err.println("Duplicate key found for owner: " + owner.getUserId() + ". Skipping insert.");
+//            }
+//          }
+//        }
+//
+////插入问题数据
+//        for (StackOverflowThread thread : stackOverflowThreads) {
+//          try {
+//            mapper.insertQuestion(thread.getQuestion()); // 调用Mapper中定义的add方法插入数据
+//          } catch (DuplicateKeyException e) {
+//            System.err.println("Duplicate key found for question: " + thread.getQuestion().getQuestionId() + ". Skipping insert.");
+//          }
+//          //插入回答数据
+//          for (Answer answer : thread.getAnswers()) {
+//            try {
+//              mapper.insertAnswer(answer);
+//            } catch (DuplicateKeyException e) {
+//              System.err.println("Duplicate key found for answer: " + answer.getAnswerId() + ". Skipping insert.");
+//            }
+//          }
+//          //插入评论数据
+//          for (Comment comment : thread.getComments()) {
+//            try {
+//              mapper.insertComment(comment, thread.getQuestion().getQuestionId());
+//            } catch (DuplicateKeyException e) {
+//              System.err.println("Duplicate key found for comment: " + comment.getCommentId() + ". Skipping insert.");
+//            }
+//          }
+//        }
+//
+//        sqlSession.commit(); // 提交事务
+//      } finally {
+//        sqlSession.close(); // 关闭SqlSession
+//      }
+//
+//    } catch (IOException e) {
+//      System.err.println("Error retrieving Stack Overflow threads: " + e.getMessage());
+//    }
+//  }
+//}
+
 @Service
 public class ThreadCollectorApp {
 
@@ -26,74 +105,76 @@ public class ThreadCollectorApp {
     ThreadCollector threadCollector = new ThreadCollector();
     String tag = "java";
     int pageSize = 1;
-    int pageNum = 1;
+    int pageNum = 10;
+    String resource = "mybatis-config.xml";
+    InputStream inputStream;
     try {
-      List<StackOverflowThread> stackOverflowThreads = new ArrayList<>();
-//        for (int i = 1; i <= pageNum; i++) {
-            stackOverflowThreads.addAll(threadCollector.getStackOverflowThreadsByTag(tag, pageSize, pageNum));
-//        }
-      String resource = "mybatis-config.xml";
-      InputStream inputStream = Resources.getResourceAsStream(resource);
+      inputStream = Resources.getResourceAsStream(resource);
       SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
       SqlSession sqlSession = sqlSessionFactory.openSession();
-      try {
-        StackOverflowThreadMapper mapper = sqlSession.getMapper(StackOverflowThreadMapper.class);
-        //插入用户数据
-        // 插入用户数据
-        Set<Owner> users = new LinkedHashSet<>();
-        for (StackOverflowThread thread : stackOverflowThreads) {
-          users.add(thread.getQuestion().getOwner());
-          for (Answer answer : thread.getAnswers()) {
-            users.add(answer.getOwner());
-          }
-          for (Comment comment : thread.getComments()) {
-            users.add(comment.getOwner());
-          }
-        }
+      StackOverflowThreadMapper mapper = sqlSession.getMapper(StackOverflowThreadMapper.class);
 
-        for (Owner owner : users) {
-          if (owner.getUserId() != null) {
-            try {
-              mapper.insertOwner(owner);
-            } catch (DuplicateKeyException e) {
-              System.err.println("Duplicate key found for owner: " + owner.getUserId() + ". Skipping insert.");
-            }
-          }
+      int successCount = 1;
+      while (successCount <= pageNum) {
+        try {
+          List<StackOverflowThread> stackOverflowThreads = threadCollector.getStackOverflowThreadsByTag(tag, pageSize, successCount);
+          processData(stackOverflowThreads, mapper);
+          sqlSession.commit(); // 提交事务
+        } catch (IOException e) {
+          System.err.println("Error retrieving Stack Overflow threads: " + e.getMessage());
+          // Retry or continue with the next iteration based on your requirements.
+            continue;
         }
-
-//插入问题数据
-        for (StackOverflowThread thread : stackOverflowThreads) {
-          try {
-            mapper.insertQuestion(thread.getQuestion()); // 调用Mapper中定义的add方法插入数据
-          } catch (DuplicateKeyException e) {
-            System.err.println("Duplicate key found for question: " + thread.getQuestion().getQuestionId() + ". Skipping insert.");
-          }
-          //插入回答数据
-          for (Answer answer : thread.getAnswers()) {
-            try {
-              mapper.insertAnswer(answer);
-            } catch (DuplicateKeyException e) {
-              System.err.println("Duplicate key found for answer: " + answer.getAnswerId() + ". Skipping insert.");
-            }
-          }
-          //插入评论数据
-          for (Comment comment : thread.getComments()) {
-            try {
-              mapper.insertComment(comment, thread.getQuestion().getQuestionId());
-            } catch (DuplicateKeyException e) {
-              System.err.println("Duplicate key found for comment: " + comment.getCommentId() + ". Skipping insert.");
-            }
-          }
-        }
-
-        sqlSession.commit(); // 提交事务
-      } finally {
-        sqlSession.close(); // 关闭SqlSession
+        successCount++;
       }
-
     } catch (IOException e) {
-      System.err.println("Error retrieving Stack Overflow threads: " + e.getMessage());
+      e.printStackTrace();
     }
   }
+
+  private static void processData(List<StackOverflowThread> stackOverflowThreads, StackOverflowThreadMapper mapper) {
+    Set<Owner> users = new LinkedHashSet<>();
+    for (StackOverflowThread thread : stackOverflowThreads) {
+      users.add(thread.getQuestion().getOwner());
+      for (Answer answer : thread.getAnswers()) {
+        users.add(answer.getOwner());
+      }
+      for (Comment comment : thread.getComments()) {
+        users.add(comment.getOwner());
+      }
+    }
+
+    for (Owner owner : users) {
+      if (owner.getUserId() != null) {
+        try {
+          mapper.insertOwner(owner);
+        } catch (DuplicateKeyException e) {
+          System.err.println("Duplicate key found for owner: " + owner.getUserId() + ". Skipping insert.");
+        }
+      }
+    }
+
+    for (StackOverflowThread thread : stackOverflowThreads) {
+      try {
+        mapper.insertQuestion(thread.getQuestion());
+      } catch (DuplicateKeyException e) {
+        System.err.println("Duplicate key found for question: " + thread.getQuestion().getQuestionId() + ". Skipping insert.");
+      }
+      for (Answer answer : thread.getAnswers()) {
+        try {
+          mapper.insertAnswer(answer);
+        } catch (DuplicateKeyException e) {
+          System.err.println("Duplicate key found for answer: " + answer.getAnswerId() + ". Skipping insert.");
+        }
+      }
+      for (Comment comment : thread.getComments()) {
+        try {
+          mapper.insertComment(comment, thread.getQuestion().getQuestionId());
+        } catch (DuplicateKeyException e) {
+          System.err.println("Duplicate key found for comment: " + comment.getCommentId() + ". Skipping insert.");
+        }
+      }
+    }
+  }
+
 }
--- src/test/java/cse/java2/project/DataAnalyzerTests.java
+++ src/test/java/cse/java2/project/DataAnalyzerTests.java
@@ -1,4 +1,114 @@
 package cse.java2.project;
 
+import cse.java2.project.service.impl.DataAnalyzer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import cse.java2.project.domain.model.dto.Question;
+import cse.java2.project.mapper.StackOverflowThreadMapper;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 public class DataAnalyzerTests {
+
+    @Autowired
+    private DataAnalyzer dataAnalyzer;
+
+    @MockBean
+    private StackOverflowThreadMapper stackOverflowThreadMapper;
+
+    @BeforeEach
+    public void setUp() {
+//        Question q1 = new Question();
+//        q1.setAnswerCount(3);
+//
+//        Question q2 = new Question();
+//        q2.setAnswerCount(0);
+//
+//        List<Question> allQuestions = Arrays.asList(q1, q2);
+//
+//        when(stackOverflowThreadMapper.getAllQuestions()).thenReturn(allQuestions);
+    }
+
+    @Test
+    public void testGetPercentageOfQuestionsWithoutAnswers() {
+        String result = dataAnalyzer.getPercentageOfQuestionsWithoutAnswers();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetAverageNumberOfAnswers() {
+        double result = dataAnalyzer.getAverageNumberOfAnswers();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetMaximumNumberOfAnswers() {
+        int result = dataAnalyzer.getMaximumNumberOfAnswers();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetDistributionOfAnswers() {
+        Map<Integer, Integer> result = dataAnalyzer.getDistributionOfNumberOfAnswers();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetPercentageOfQuestionsWithAcceptedAnswers() {
+        String result = dataAnalyzer.getPercentageOfQuestionsWithAcceptedAnswers();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testDistributionOfQuestionResolutionTime() {
+        Map<Long, Integer> result = dataAnalyzer.getDistributionOfQuestionResolutionTime();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testPercentageOfQuestionsWithNonAcceptedAnswersHavingMoreUpvotes() {
+        String result = dataAnalyzer.getPercentageOfQuestionsWithNonAcceptedAnswersHavingMoreUpvotes();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetFrequentTagsWithJava() {
+        Map<String, Integer> result = dataAnalyzer.getFrequentTagsWithJava();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetMostUpvotedTagsOrTagCombinations() {
+        Map<String, Integer> result = dataAnalyzer.getMostUpvotedTagsOrTagCombinations();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetMostViewedTagsOrTagCombinations() {
+        Map<String, Integer> result = dataAnalyzer.getMostViewedTagsOrTagCombinations();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetDistributionOfUserParticipation() {
+        Map<Integer, Integer> result = dataAnalyzer.getDistributionOfUserParticipation();
+        System.out.println(result);
+    }
+
+    @Test
+    public void testGetMostActiveUsers() {
+        List<String> result = dataAnalyzer.getMostActiveUsers();
+        System.out.println(result);
+    }
 }
+
